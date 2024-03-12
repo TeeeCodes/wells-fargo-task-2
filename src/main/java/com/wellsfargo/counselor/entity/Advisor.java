038403571698bf51cc4cebc2d7ecb70f0de39c7a
TeeeCodes <Terencepriesterr@gmail.com>
2024-03-12 01:17:43 -0400

    entities changed

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,10 +1,16 @@
 package com.wellsfargo.counselor.entity;
 
 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 public class Advisor {
